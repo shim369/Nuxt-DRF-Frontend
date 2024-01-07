f243e5e778675032816206738524dd0d527c1e3e
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-07 20:12:18 +0900

    update tailwind

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -14,7 +14,7 @@ useSeoMeta({
 
 <template>
     <UnderTitle under_h1="Project Detail" />
-    <div class="py-10 px-6 w-full md:w-3/5 m-auto bg-[#3a3a3a] mt-10 border-b-8 border-[#e82c2c]" v-if="project">
+    <div class="py-10 px-6 w-[90%] md:w-[60%] m-auto bg-[#3a3a3a] mt-10 border-b-8 border-[#e82c2c]" v-if="project">
         <div class="m-auto md:col-span-3">
             <h2 class="mb-6 text-2xl">{{ project.title }}</h2>
             <img :src="project.image_url" class="max-h-[400px] w-full object-cover" :alt="project.title">
