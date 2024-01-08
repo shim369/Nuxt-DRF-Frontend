01c7a96600ec863da375e038ce0a6f41b5753340
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-08 15:14:29 +0900

    update tailwind & text

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -18,10 +18,10 @@ useSeoMeta({
         <div class="m-auto md:col-span-3">
             <h2 class="mb-6 text-2xl">{{ project.title }}</h2>
             <img :src="project.image_url" class="max-h-[400px] w-full object-cover" :alt="project.title">
-            <div class="mt-6 text-xl">{{ project.content }}</div>
+            <div class="mt-6 text-base">{{ project.content }}</div>
             <div class="mt-10">
                 <a :href="project.demo_link" target="_blank" rel="noopener noreferrer"
-                    class="btn-gray-rounded mr-2">Site</a>
+                    class="btn-gray-rounded mr-2">URL</a>
                 <a :href="project.github_repo" target="_blank" rel="noopener noreferrer"
                     class="btn-gray-rounded">GitHub</a>
             </div>
