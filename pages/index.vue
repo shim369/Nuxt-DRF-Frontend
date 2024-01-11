705fcc9aebc613e4d76e87f4f18be81a4ebb0d97
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-11 15:44:40 +0900

    update NuxtImg

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@ useSeoMeta({
                     <div class="py-8">
                         <NuxtImg format="webp" src="/pongki.webp" alt="pongki"
                             class="rounded-full max-w-[150px] w-[100%] max-h-[150px] h-[100%] m-auto mb-6" width="150"
-                            height="150" />
+                            height="150" sizes="(max-width: 640px) 100px, 150px" />
                     </div>
                     <p class="text-center mb-2">S-Ohtani</p>
                     <p class="text-center">Passionate developer</p>
